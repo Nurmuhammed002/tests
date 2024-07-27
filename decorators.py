889f4054841b404ffd2b85c80c885b814ccc5451
Nurmuhammed002 <mzhumashev509@gmail.com>
2024-07-28 00:39:14 +0600

    Add decorator in decorators.py

---
+++ decorators.py
@@ -0,0 +1,15 @@
+
+def simple_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Before function call")
+        result = func(*args, **kwargs)
+        print("After function call")
+        return result
+    return wrapper
+
+@simple_decorator
+def say_hello(name):
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    say_hello("World")
